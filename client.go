7361f98f0e9c6688ae32851fce37ae7e2460f5e3
Keano Porcaro <keano.porcaro@protonmail.com>
2019-02-05 16:31:12 +1100

    Update client.go

--- client.go
+++ client.go
@@ -1,33 +1,84 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "net"
+  "flag"
   "bufio"
+  "net"
+  "fmt"
+  "os"
+  "strconv"
+  "regexp"
+  "time"
 )
 
-func main() {
-  p := make([]byte, 2048)
-  conn, err := net.Dial("udp", "127.0.0.1:666")
-  var arg = "nil"
-  if err != nil {
-    fmt.Printf("Something went wrong %v\n", err)
-    return
+var host = flag.String("host", "localhost", "Host to connect to; defaults to \"localhost\".")
+var port = flag.Int("port", 8080, "The port to connect to; defaults to 8080.")
+
+func readConnection(conn net.Conn) {
+  for {
+    scanner := bufio.NewScanner(conn)
+    
+    for {
+      ok := scanner.Scan()
+      text := scanner.Text()
+      
+      command := handleCommands(text)
+      if !command {
+        fmt.Printf("\b\b** %s\n> ", text)
+      }
+      
+      if !ok {
+        fmt.Println("EOF Server connection.")
+        break
+      }
+    }
   }
-  
-  if len(os.Args) > 1 {
-    arg = os.Args[1]
-  } else {
-    fmt.Printf("Missing a message argument, using default message\n")
-  }
-  
-  fmt.Printf(conn, arg)
-  _, err = bufio.NewReader(conn).Read(p)
+}
+
+func handleCommands(text string) bool {
+  r, err := regexp.Compile("^%.*%$")
   if err == nil {
-    fmt.Printf("%s\n", p)
-  } else {
-    fmt.Printf("Something went wrong %v\n", err)
+    if r.MatchString(text) {
+      switch {
+      case text == "%quit%":
+        fmt.Println("\b\b[Server] disconnecting..")
+        os.Exit(0)
+      }
+      return true
+    }
   }
-  conn.Close()
+  return false
+}
+
+func main() {
+  flag.Parse()
+  
+  dest := *host + ":" + strconv.Itoa(*port)
+  fmt.Printf("[Client] Connecting to %s...\n", dest)
+  
+  conn, err := net.Dial("tcp", dest)
+  
+  if err != nil {
+    if _, t := err.(*net.OpError); t{
+      fmt.Println("Error connecting...")
+    } else {
+      fmt.Println("Unknown error: " + err.Error())
+    }
+    os.Exit(1)
+  }
+  
+  go readConnection(conn)
+
+  for {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("> ")
+    text, _ := reader.ReadString('\n')
+    
+    conn.setWriteDeadline(time.Now().Add(1 * time.Second))
+    _, err := conn.Write([]byte(text))
+    if err != nil {
+      fmt.Println("Error writing to stream.")
+      break
+    }
+  }
 }
